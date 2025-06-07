7222801de20092a4cd4fc6181b6116dd0a919a02
Max.ggre <max.ggre@gmail.com>
2025-06-08 01:30:29 +0200

    cleaned resources

--- Database/resources.py
+++ Database/resources.py
@@ -4,17 +4,17 @@ foxholeversion = "V61"
 ue_ver = "VER_UE4_27"
 
 # main folder \Automod_txt
-automodtxt_path = r"C:\Users\Max\Documents\Automod_txt" #CUSTOM
+automodtxt_path = r"" #CUSTOM
 
 # Where stuff will be stored
 version_folder = os.path.join(automodtxt_path, "Result", foxholeversion)
 
 # Where fmodel store   \Output\Exports
-Fmodel_output_folder = r"C:\Users\Max\Downloads\DLs\Output\Exports" #CUSTOM
+Fmodel_output_folder = r"" #CUSTOM
 
 
 # Template tool for .pak   \Template
-template_folder = r"C:\Users\Max\Documents\Automod_txt\Template" #CUSTOM
+template_folder = r"" #CUSTOM
 war_pak_folder = os.path.join(template_folder, r"War")
 
 locres_pak_folder = os.path.join(template_folder, r"War\Content\Localization") 
@@ -27,9 +27,9 @@ bat_path = os.path.join(template_folder, "run.bat")
 ### .uasset resources
 
 style_folder = os.path.join(automodtxt_path, r"Database\Style")
-tool_executable = r"D:\backup\Image\modding\Utilities-master" #CUSTOM do not include UAssetGUI.exe in the path
+tool_executable = r"" #CUSTOM do not include UAssetGUI.exe in the path
 
 ### .locres resources \Utilities-master
 
-tool_path = r"D:\backup\Image\modding\Utilities-master" #CUSTOM do not include UE4LocalizationsTool.exe in the path
+tool_path = r"" #CUSTOM do not include UE4LocalizationsTool.exe in the path
 
